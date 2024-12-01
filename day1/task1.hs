ca6d75bc9c0901ec2fb66c635857c0107a663d01
GooRoo <sergey.olendarenko@gmail.com>
2024-12-01 15:09:50 +0100

    style: Use some monadic trash

--- day1/task1.hs
+++ day1/task1.hs
@@ -1,20 +1,24 @@
 import Data.List (sort)
+import Data.Functor ( (<&>) )
 import Data.Tuple.Extra (both)
 import System.Environment (getArgs)
 import System.IO
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let filename = head args
-  contents <- readFile filename
-  let (left, right) = both sort . unzip . map parseLine $ lines contents
+  (left, right) <- both sort <$> parseFile
   print $ sum $ zipWith distance left right
+
+distance :: Int -> Int -> Int
+distance l r = abs $ l - r
+
+parseFile :: IO ([Int], [Int])
+parseFile = getArgs >>= readFile . head <&> parse
+
+parse :: String -> ([Int], [Int])
+parse = unzip . map parseLine . lines
 
 parseLine :: String -> (Int, Int)
 parseLine line =
   let [x, y] = map read (words line)
    in (x, y)
-
-distance :: Int -> Int -> Int
-distance l r = abs $ l - r
