27d2c28e4a9d07cb55145050908f0d4bf5bb244f
GooRoo <sergey.olendarenko@gmail.com>
2024-12-01 15:09:50 +0100

    style: Use some monadic trash

--- day1/task2.hs
+++ day1/task2.hs
@@ -1,21 +1,25 @@
+import Data.Functor ( (<&>) )
 import Data.List.Utils (countElem)
 import System.Environment (getArgs)
 import System.IO
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let filename = head args
-  contents <- readFile filename
-  let (left, right) = unzip . map parseLine $ lines contents
+  (left, right) <- parseFile
   print $ similarity right left
-
-parseLine :: String -> (Int, Int)
-parseLine line =
-  let [x, y] = map read (words line)
-   in (x, y)
 
 similarity :: [Int] -> [Int] -> Int
 similarity r = sum . map score
   where
     score x = x * countElem x r
+
+parseFile :: IO ([Int], [Int])
+parseFile = getArgs >>= readFile . head <&> parse
+
+parse :: String -> ([Int], [Int])
+parse = unzip . map parseLine . lines
+
+parseLine :: String -> (Int, Int)
+parseLine line =
+  let [x, y] = map read (words line)
+   in (x, y)
